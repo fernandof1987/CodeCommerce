42e889670b6e975ba15685335873b3b8bdce9df4
Fernando <fernandof1987@gmail.com>
2015-05-25 22:56:16 -0300

    Model Product ad ProductsController

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,46 @@
+<?php namespace CodeCommerce\Http\Controllers;
+
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller {
+
+
+	public function index()
+	{
+		//
+	}
+
+	public function create()
+	{
+		//
+	}
+
+	public function store()
+	{
+		//
+	}
+
+	public function show($id)
+	{
+		//
+	}
+
+	public function edit($id)
+	{
+		//
+	}
+
+	public function update($id)
+	{
+		//
+	}
+
+	public function destroy($id)
+	{
+		//
+	}
+
+}
